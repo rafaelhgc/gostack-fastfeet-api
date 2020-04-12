9043ff69b637efc6401c167989dd8c388b4f71ce
Rafael Costa <gomes.rafaelcosta@gmail.com>
2020-04-12 09:48:53 -0300

    readme

--- src/database/seeds/20200213005120-restrictions.js
+++ src/database/seeds/20200213005120-restrictions.js
@@ -5,50 +5,50 @@ module.exports = {
       [
         {
           day_of_week: 0, // Sunday
-          start_at: '08:00',
-          end_at: '18:00',
+          start_at: '06:00',
+          end_at: '12:00',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
           day_of_week: 1, // Monday
-          start_at: '08:00',
-          end_at: '18:00',
+          start_at: '06:00',
+          end_at: '20:00',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
           day_of_week: 2, // Tuesday
-          start_at: '08:00',
-          end_at: '18:00',
+          start_at: '06:00',
+          end_at: '20:00',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
           day_of_week: 3, // Wednesday
-          start_at: '08:00',
-          end_at: '18:00',
+          start_at: '06:00',
+          end_at: '20:00',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
           day_of_week: 4, // Thursday
-          start_at: '08:00',
-          end_at: '18:00',
+          start_at: '06:00',
+          end_at: '20:00',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
           day_of_week: 5, // Friday
-          start_at: '08:00',
-          end_at: '18:00',
+          start_at: '06:00',
+          end_at: '20:00',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
           day_of_week: 6, // Saturday
-          start_at: '08:00',
-          end_at: '18:00',
+          start_at: '06:00',
+          end_at: '12:00',
           created_at: new Date(),
           updated_at: new Date(),
         },
